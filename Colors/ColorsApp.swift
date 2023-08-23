236c454d947974a1594b344a27820432092a4251
Kubomi <tanakara9botamochi@gmail.com>
2023-08-23 12:21:41 +0900

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by 久保田麻美 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Colors
+//
+//  Created by 久保田麻美 on 2023/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
