921c75f481cdacb123ab7105bd25620b6e27b6a0
Kubomi <tanakara9botamochi@gmail.com>
2023-08-23 12:26:23 +0900

    Create ColorModel.swift

---
+++ Model/ColorModel.swift
@@ -0,0 +1,18 @@
+//
+//  ColorModel.swift
+//  Colors
+//
+//  Created by 久保田麻美 on 2023/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct MyColor: Identifiable {
+    var id = UUID()
+    var name_jp: String
+    var name_kana: String
+    var name_eng: String
+    var color: Color
+    var detail: String
+}
