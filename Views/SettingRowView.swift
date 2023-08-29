a3ab04693abd3d2935c2d7943ece33b8c6e93a56
Kubomi <tanakara9botamochi@gmail.com>
2023-08-29 16:04:11 +0900

    update settings view

---
+++ Views/SettingRowView.swift
@@ -0,0 +1,51 @@
+//
+//  SettingRowView.swift
+//  Colors
+//
+//  Created by 久保田麻美 on 2023/08/29.
+//
+
+import SwiftUI
+
+struct SettingRowView: View {
+    // Properties
+    var name: String
+    var content: String? = nil
+    var linkLabel: String? = nil
+    var linkDest: String? = nil
+    
+    var body: some View {
+        VStack {
+//            Divider()
+//                .padding(.vertical, 4)
+            
+            HStack {
+                Text(name)
+                    .foregroundColor(.secondary)
+                Spacer()
+                if content != nil {
+                    Text(content!)
+                } else if (linkLabel != nil && linkDest != nil) {
+                    Link(linkLabel!, destination: URL(string: linkDest!)!)
+                    Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+                } else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                }
+            }
+            .padding(.vertical, 8)
+        }
+    }
+}
+
+struct SettingRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SettingRowView(name: "Developer", content: "Asami")
+                .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
+            SettingRowView(name: "Link", linkLabel: "Website", linkDest: "test.com")
+                .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
+        }
+    }
+}
